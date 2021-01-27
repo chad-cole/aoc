a271e7fe38a9f361b689c9bdd3c808775496363a
Chad Cole <cjcole92@gmail.com>
2021-01-27 17:05:10 -0500

    cleaned up interval tree day16 and added pypi intervaltree to env

--- day16.py
+++ day16.py
@@ -25,7 +25,7 @@ data = Puzzle(year=2020, day=16).input_data
 
         - In either case, I then have to iterate through the alternate values and sum
 
-        - Let's try the interval tree (Don't worry about balancing for now and it's more of a  web)
+        - Let's try the interval tree (using pypy IntervalTree)
             - Invariant of the tree is that each node holds a min and max for an interval
                 to the left are intervals that start lower than current, and to the right are intervals 
                 that end higer than current
@@ -42,49 +42,10 @@ data = Puzzle(year=2020, day=16).input_data
         - return the product in your ticket using the new field names
         
 """
-from functools import lru_cache, reduce
+
+from functools import reduce
 from itertools import chain, count
-
-class inode:
-
-    def __init__(self, interval, fields, left=None, sub=None, right=None):
-        self.int = interval
-        self.fields = fields
-        self.left = left
-        self.right = right
-        self.sub = sub
-
-    def add_node(self, node):
-        mn, mx, imn, imx = *self.int, *node.int
-        if mn == imn and mx == imx: self.fields.extend(node.fields)
-        if imn <= mn and imx >= mx: #new node is superset, swap nodes
-            self.fields.update(node.fields)
-            self.fields, node.fields = node.fields, self.fields
-            self.int, node.int = node.int, self.int
-            if self.sub: self.sub.add_node(node)
-            else: self.sub = node
-        elif mn <= imn and mx >= imx: #new node is a subset 
-            node.fields.update(self.fields)
-            if self.sub: self.sub.add_node(node)
-            else: self.sub = node
-        elif imn < mn: 
-            if self.left: self.left.add_node(node)
-            else: self.left = node
-        elif imx > mx: 
-            if self.right: self.right.add_node(node)
-            else: self.right = node
-
-    @classmethod
-    @lru_cache
-    def find(cls, node, n):
-        if not node: return set()
-        mn, mx = node.int
-        left = inode.find(node.left, n)
-        right = inode.find(node.right, n)
-        output = set.union(left, right)
-        if mn <= n <= mx: 
-            output = set.union(node.fields, inode.find(node.sub, n), output)
-        return output
+from intervaltree import IntervalTree
 
 """
 Parse the Input Data
@@ -93,17 +54,14 @@ with open("short.txt", 'r') as f:
     #data = f.read().rstrip()
     pass
 
-itree = None
+inttree = IntervalTree()
 
 rules, mine, alts = data.split('\n\n')
-
 for rule in rules.split('\n'):
     field, bounds = map(str.strip, rule.split(':'))
     for bound in map(str.strip, bounds.split('or')):
         mn, mx = map(int, map(str.strip, bound.split('-')))
-        node = inode((mn, mx), {field})
-        if not itree: itree = node
-        else: itree.add_node(node)
+        inttree[mn:mx+1] = field
 
 alts = [[int(x) for x in alt.split(',')] for alt in alts.split('\n')[1:]]
 mine = [[int(x) for x in alt.split(',')] for alt in mine.split('\n')[1:]]
@@ -112,7 +70,7 @@ mine = [[int(x) for x in alt.split(',')] for alt in mine.split('\n')[1:]]
 Part 1
 """
 
-part1 = sum(x for x in chain.from_iterable(alts) if not inode.find(itree, x))
+part1 = sum(x for x in chain.from_iterable(alts) if not inttree[x])
 print(f"Part 1: {part1}")
 
 """
@@ -125,12 +83,12 @@ Part 2
     - return the product in your ticket using the new field names
 """
 
-alts_clean = [x for x in alts+mine if all(inode.find(itree, y) for y in x)]
-field_names = [set.intersection(*[inode.find(itree, x[i]) for x in alts_clean]) for i in range(len(alts[0]))]
-
+alts_clean = [x for x in alts+mine if all(inttree[y] for y in x)]
+field_names = [set.intersection(*[set(x.data for x in inttree[x[i]]) for x in alts_clean]) for i in range(len(alts[0]))]
 sorted_counted = sorted(zip(field_names, count()), key=lambda x: len(x[0]))
-taken, fields = set(), dict()
+ 
 
+taken, fields = set(), dict()
 for s, i in sorted_counted:
     key = (s-taken).pop()
     fields[key] = i
